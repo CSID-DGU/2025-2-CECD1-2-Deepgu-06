7c588f66ed670e9d906bf78999070e336a4da74d
kiyeonkimm <paynjoy2@naver.com>
2025-11-15 20:22:35 +0900

    📌 Feat    : CCTV 페이지 컴포넌트 퍼블리싱

---
+++ FE/deepgu/app/cctv/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Image from 'next/image';
+import AlertPanel from '@/components/alert';
+import Txt from '@/components/atoms/Text';
+import CCTVPanel from '@/components/cctv';
+import LogPanel from '@/components/log';
+
+export default function Dashboard() {
+  return (
+    <div className='mx-auto flex w-full max-w-5xl flex-col gap-6'>
+      {/* 로고 */}
+      <div className='flex items-center gap-3'>
+        <Image
+          src='/icons/logo.svg'
+          alt='로고'
+          width={56}
+          height={56}
+          priority
+        />
+        <Txt weight='semibold' className='text-Hana-Black text-[32px]'>
+          이상행동 감지 시스템
+        </Txt>
+      </div>
+
+      {/* 메인 레이아웃 */}
+      <div className='flex gap-6'>
+        <CCTVPanel />
+        <aside className='flex w-72 flex-col gap-4'>
+          <AlertPanel />
+          <LogPanel />
+        </aside>
+      </div>
+    </div>
+  );
+}
---
+++ FE/deepgu/components/log.tsx
@@ -0,0 +1,12 @@
+import Txt from './atoms/Text';
+
+export default function LogPanel() {
+  return (
+    <div className='bg-Gray rounded-2xl p-4 text-white shadow-lg'>
+      <Txt weight='semibold' className='text-xl text-white'>
+        최근 통계
+      </Txt>
+      <div className='bg-Semi-Red mt-3 h-10 w-full rounded-xl pt-3 shadow-inner' />
+    </div>
+  );
+}
